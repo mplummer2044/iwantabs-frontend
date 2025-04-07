f435941c410922b1f6af17125e7e8fa831afac13
map.lummer <mattjohnplummer@gmail.com>
2025-04-07 19:15:36 -0400

    Troubleshooting auth more

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,16 @@ const awsConfig = {
   Auth: {
     Cognito: {
       userPoolId: 'us-east-1_lcZMlbm3c',
-      userPoolClientId: '68peajep7rg98ti9dtr76q8s1l'
+      userPoolClientId: '68peajep7rg98ti9dtr76q8s1l',
+      loginWith: {
+        oauth: {
+          redirectSignIn: ['https://main.d1atihsq0v31p5.amplifyapp.com'],
+          redirectSignOut: ['https://main.d1atihsq0v31p5.amplifyapp.com'],
+          responseType: 'code'
+        }
+      }
     }
   }
 };
+
+export default awsConfig;
