185cd54f54d5c611d5315ad10a9d577656af3af0
map.lummer <mattjohnplummer@gmail.com>
2025-04-17 09:47:06 -0400

    Revert "troubleshooting build failures"
    This reverts commit 17b95fbdae44ef4b8969b01cac0dd7b46b3d7ba4.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { fetchAuthSession } from 'aws-amplify/auth';
