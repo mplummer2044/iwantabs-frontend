bf6968edca9d6252f42397cfb066e99099a9df83
map.lummer <mattjohnplummer@gmail.com>
2025-06-04 08:53:51 -0400

    fixing input fields

--- src/components/ActiveWorkout/SwipeContainer.jsx
+++ src/components/ActiveWorkout/SwipeContainer.jsx
@@ -9,15 +9,15 @@ const SwipeContainer = ({ children, currentIndex }) => {
     return (
       <div className="swipe-container">
         <AnimatePresence initial={false} custom={currentIndex}>
-          <motion.div
-            key={currentIndex}
-            custom={currentIndex}
-            initial={{ y: 300, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -300, opacity: 0 }}
-            transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-            className="card-wrapper"
-          >
+            <motion.div
+                key={currentIndex}
+                custom={currentIndex}
+                initial={{ y: 50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -50, opacity: 0 }}
+                transition={{ type: 'spring', stiffness: 100, damping: 25 }}
+                className="card-wrapper"
+                >
             {children[currentIndex] || <div>No content</div>}
           </motion.div>
         </AnimatePresence>
