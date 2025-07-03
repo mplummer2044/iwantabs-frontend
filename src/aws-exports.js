8fc528d7531418360716cc5e869462a8abc712a0
map.lummer <mattjohnplummer@gmail.com>
2025-07-02 20:22:12 -0400

    fixing auth

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -9,17 +9,13 @@ const awsConfig = {
     ]
   },
   Auth: {
-    Cognito: {
-      userPoolId: 'us-east-1_lcZMlbm3c',
-      userPoolClientId: '5p646eiimag416fhhai5bhgrf6',
-      loginWith: {
-        oauth: {
-          redirectSignIn: ['https://main.d1atihsq0v31p5.amplifyapp.com'],
-          redirectSignOut: ['https://main.d1atihsq0v31p5.amplifyapp.com'],
-          responseType: 'code'
-        }
-      }
-    }
+    // REQUIRED - Amazon Cognito Region
+    region: "us-east-1",
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: "us-east-1_lcZMlbm3c",
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: "5p646eiimag416fhhai5bhgrf6"
+    // (If you need OAuth flows, you can add an `oauth` block here)
   }
 };
 
