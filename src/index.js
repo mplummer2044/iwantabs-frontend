a47750da710004e31d01b7068293115447d19225
map.lummer <mattjohnplummer@gmail.com>
2025-07-03 11:36:52 -0400

    fixing auth

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 import awsConfig from './aws-exports';
-// Configure Amplify with AWS Cognito/API settings
-Amplify.configure(awsConfig);
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { WorkoutProvider } from './components/common/WorkoutContext';
+
+// Configure Amplify with AWS Cognito/API settings
+Amplify.configure(awsConfig);
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
