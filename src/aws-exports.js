2e9f19a81507b1f830051873441bfb56b3e1ffac
map.lummer <mattjohnplummer@gmail.com>
2025-07-05 13:35:43 -0400

    auth fix still

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,21 +1,12 @@
 const awsConfig = {
     Auth: {
-      // Region where your Cognito User Pool lives
+      // Required for Cognito User Pool auth
       region: 'us-east-1',
-      // Your User Pool ID
       userPoolId: 'us-east-1_lcZMlbm3c',
-      // Your User Pool App Client ID
       userPoolWebClientId: '5p646eiimag416fhhai5bhgrf6',
-      // Optional: require signin for every API call
       mandatorySignIn: false,
-      // If you later add Hosted UI / OAuth, you can enable:
-      // oauth: {
-      //   domain: 'your-domain.auth.us-east-1.amazoncognito.com',
-      //   scope: ['openid','email','profile'],
-      //   redirectSignIn: 'https://main.d1atihsq0v31p5.amplifyapp.com/',
-      //   redirectSignOut: 'https://main.d1atihsq0v31p5.amplifyapp.com/',
-      //   responseType: 'code'
-      // },
+     // Even if you’re not using hosted UI, Amplify UI code expects this key to exist
+      oauth: {}
     },
     API: {
       endpoints: [
